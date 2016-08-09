acf6806bf4d6e11405dcdc440cbdd59ee60a52e2
Wagner Ferreira <wcarvalho.ferreira>
2016-08-09 09:15:54 -0300

    It adds log to the module

--- raspbel/logbel.py
+++ raspbel/logbel.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import csv
+import logging
 import os.path
+from define import Define
 from parser import *
 
-def get_row(frame):
-	p = Parser(frame)
+def init_log():
+	# create logger
+	log = logging.getLogger(__name__)
+	log.setLevel(logging.DEBUG)
+	log.disabled = Define.PARSER_DEBUG_DISABLED
+	# create console handler and set level to debug
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.DEBUG)
+
+	# create formatter
+	formatter = logging.Formatter("%(asctime)s - [ %(levelname)-8s] : %(message)s", "%Y-%m-%d %H:%M:%S")
+
+	# add formatter to ch
+	ch.setFormatter(formatter)
+
+	# add ch to logger
+	log.addHandler(ch)
+	return log
+
+def get_row(p):
+	
 	bel_list = [( p.get_num_pacs(), p.get_num_serie(), p.get_id_radio(), p.get_cod_equipamento(), p.get_id_cba(), \
 		          p.get_personalidade(), p.get_versao_fw(), p.get_estado_cec(), p.get_estado_ciclo(), \
 	              p.get_estado_maquina(), p.get_data_hora(), p.get_speed(), p.get_heading(), \
@@ -14,6 +35,8 @@ def get_row(frame):
 	return bel_list
 
 def log_bel(frame):
+	log = init_log()
+	p = Parser(frame)
 	file_exists = os.path.isfile('bel.csv')
 
 	with open('bel.csv', 'a') as belfile:	
@@ -28,4 +51,5 @@ def log_bel(frame):
 		if not file_exists:
 			f_bel.writerow(headers)
 
-		f_bel.writerows(get_row(frame))
+		log.debug("Receiving Bel package from radio: {}".format(p.get_id_radio()))
+		f_bel.writerows(get_row(p))
