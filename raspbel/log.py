6f0e34733eb2e06e43fd3a69d0a38a170f52bf9d
Wagner Ferreira <wcarvalho.ferreira>
2016-08-11 08:11:58 -0300

    Adds filename, linenumber and function name to the log

--- raspbel/log.py
+++ raspbel/log.py
@@ -15,7 +15,8 @@ class Logger:
 		ch.setLevel(logging.DEBUG)
 
 		# create formatter
-		formatter = logging.Formatter("%(asctime)s - [ %(levelname)-8s] : %(message)s", "%Y-%m-%d %H:%M:%S")
+		formatter = logging.Formatter("%(asctime)-8s - %(filename)s:%(lineno)3s - %(funcName)10s() - %(levelname)8s - %(message)s",
+										"%Y-%m-%d %H:%M:%S") 
 
 		# add formatter to ch
 		ch.setFormatter(formatter)
