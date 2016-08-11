64773436ec28877f79e557b31d377f4a4c94192d
Wagner Ferreira <wcarvalho.ferreira>
2016-08-11 10:05:49 -0300

    Adding more log constants

--- raspbel/beldefine.py
+++ raspbel/beldefine.py
@@ -7,5 +7,8 @@ class Define:
 	BEL = 0x07
 	FRAME_ID = 0x90
 	ESCAPE = 0x7d
-	FSM_DEBUG_DISABLED = False     # False: Log enabled True: Log disabled
-	PARSER_DEBUG_DISABLED = False # False: Log enabled True: Log disabled
+	BELBD_DEBUG_DISABLED = True       # False: Log enabled True: Log disabled
+	BELCSV_DEBUG_DISABLED = True      # False: Log enabled True: Log disabled
+	BELFSM_DEBUG_DISABLED = True      # False: Log enabled True: Log disabled	
+	BELLOG_DEBUG_DISABLED = False     # False: Log enabled True: Log disabled
+	BELPARSER_DEBUG_DISABLED = True   # False: Log enabled True: Log disabled
