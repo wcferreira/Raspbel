edda4074d0d02a5b88baa5c1db01e108368de6fc
Wagner Ferreira <wcarvalho.ferreira>
2016-08-11 10:11:14 -0300

    Improves the log format.

--- raspbel/bellogger.py
+++ raspbel/bellogger.py
@@ -15,7 +15,7 @@ class Logger:
 		ch.setLevel(logging.DEBUG)
 
 		# create formatter
-		formatter = logging.Formatter("%(asctime)-8s - %(filename)s:%(lineno)3s - %(funcName)10s() - %(levelname)8s - %(message)s",
+		formatter = logging.Formatter("%(asctime)-8s - %(filename)s:%(lineno)3s - %(funcName)20s() - %(levelname)8s - %(message)s",
 										"%Y-%m-%d %H:%M:%S") 
 
 		# add formatter to ch
